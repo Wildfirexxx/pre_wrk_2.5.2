d900c96b24761c43820faf1a687431e45ebd2c2c
Alderick <alderick@aldericks-mbp.lan>
2021-12-25 05:51:30 -0500

    prework 2.5.2 watch the box

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("shrink").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "50px";
+
+        });
+
+        document.getElementById("grow").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "300px";
+
+        });
+
+        document.getElementById("reset").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "200px";
+
+        });
